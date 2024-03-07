63d1a88b5ea699ed6702bff647317c1001e142b2
Noah Struck <noahstruck71303@gmail.com>
2024-03-06 21:45:14 -0500

    Refactored some names, worked on finishing header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,33 @@
-import Repos from "@/components/Repos";
+import Projects from "@/components/server/Projects";
 import { Suspense } from "react";
 
-export default async () => {
+export default () => {
   return (
-    <main className="flex min-h-screen flex-col gap-24 justify-center p-24 bg-gray-200">
-      <div className="flex flex-col items-center h-full w-full">
-          <div>
-            <p className="text-3xl font-bold">Noah Struck</p>
-            <p>Software Engineer</p>
+    <main className="bg-gray-200 px-32 2xl:px-96">
+
+      {/* Header */}
+      <div className="min-h-screen justify-center">
+        <div className="flex h-screen justify-center items-center">
+          <div className="flex flex-col gap-4 text-center">
+            <h1 className="text-5xl font-bold">Noah Struck</h1>
+            <div className="flex flex-row gap-1">
+              <p>CS @ Kent</p>
+              <p className="self-center text-xs">|</p>
+              <p>Software Engineer</p>
+              <p className="self-center text-xs">|</p>
+              <p>Hobbyist</p>
+            </div>
           </div>
-      </div>
-      <div className="flex flex-col items-center gap-5 h-full w-full">
-        <Suspense fallback={<p>Loading repos..</p>}>
-          <Repos />
-        </Suspense>
+        </div>
+
+        {/* Projects */}
+        <div className="flex flex-col gap-2 h-full w-full pb-20">
+          <h1 className="text-3xl font-bold">Projects</h1>
+          <p>Here are some of my project's I've worked on. Currently, they are dynamically loaded from Github. In the future, this website will load nothing from Github and will just be statically served.</p>
+          <Suspense fallback={<p>Loading projects..</p>}>
+            <Projects />
+          </Suspense>
+        </div>
       </div>
     </main>
   );
--- src/components/server/Projects.tsx
+++ src/components/server/Projects.tsx
@@ -1,14 +1,13 @@
 "use server";
 
 import { getRepos } from "@/lib/github";
-import { Card } from "./Repo";
+import { Project } from "../Project";
 
 export default async () => {
-
     const repos = await getRepos();
     return (
         <div className="grid grid-cols-3 gap-4">
-            {repos.map((repo, index) => <Card key={repo.id} index={index} {...repo} />)}
+            {repos.map(repo => <Project key={repo.id} {...repo} />)}
         </div>
     )
 }
