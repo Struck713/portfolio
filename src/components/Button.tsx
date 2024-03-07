a0388a3f1db86590785f8d329b3369b9aa7edcd5
Noah Struck <noahstruck71303@gmail.com>
2024-03-07 15:54:02 -0500

    Messed with button styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,6 +2,8 @@ import { HTMLProps } from "react";
 
 export default ({ className, children, ...props }: HTMLProps<HTMLAnchorElement>) => {
     return (
-        <a {...props} className={`${className} bg-gray-500 text-white font-bold py-2 px-4 rounded`}>{children}</a>
+        <a {...props} className={`${className} bg-none text-black font-bold py-2 px-4 rounded transition-colors ease-in-out duration-300 hover:bg-gray-300`}>
+            {children}
+        </a>
     )
 }
