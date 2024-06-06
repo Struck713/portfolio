90279ce994ca14c1eed216c3bc57cd7cb0df1d7b
Struck713 <noahstruck71303@gmail.com>
2024-06-05 21:46:24 -0400

    Fixed skills section (and didn't add any)

--- src/components/sections/Experience.tsx
+++ src/components/sections/Experience.tsx
@@ -5,19 +5,24 @@ import { Utils } from "@/lib/util";
 import moment from "moment";
 import { ReactNode } from "react";
 import Section from "./Section";
-import { IconLanguages } from "../Icons";
 
 export interface Experience {
     logo: ReactNode,
     name: string,
     title: string,
     url: string,
-    skills: IconLanguages[],
+    skills: string[],
     description: ReactNode,
     start: string,
     end?: string
 }
-export const Experience = ({ title, logo, description, url, start, end }: Experience) => {
+
+interface PillProps { title: string };
+export const Pill = ({ title }: PillProps) => {
+    return <span className="text-xs bg-white rounded-lg p-1">{title}</span>
+}
+
+export const Experience = ({ title, logo, description, url, start, end, skills }: Experience) => {
 
     const getDateRange = (start: string, end?: string) => {
         const startMoment = moment(start);
@@ -28,7 +33,7 @@ export const Experience = ({ title, logo, description, url, start, end }: Experi
     }
 
     return (
-        <div className={`px-10 py-5 bg-primary rounded-lg drop-shadow-md`}>
+        <div className="px-10 py-5 bg-primary rounded-lg drop-shadow-md">
             <div className="flex flex-col gap-2">
                 <div className="flex flex-row justify-between">
                     <div className="flex flex-col gap-1">
@@ -38,6 +43,11 @@ export const Experience = ({ title, logo, description, url, start, end }: Experi
                     <a className="self-center" href={url} target="_blank">{logo}</a>
                 </div>
                 <div className="w-full">{description}</div>
+                {skills.length != 0 && 
+                    <div className="flex flex-row gap-1 pt-1">
+                        {skills.map(pill => <Pill title={pill} />)}
+                    </div>
+                }
             </div>
         </div>
     )
